5c44524aed256c86a34ae55ad07f56df5a539a8c
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-10-14 13:09:53 +0200

    update vercel config

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://mypearlsofgreatprice.vercel.app'], // Vercel domain
+    'allowed_origins' => ['https://mypearlsofgreatprice.vercel.app', 'https://api-memories.vercel.app'], 
 
     // 'allowed_origins' => ['*'],
 

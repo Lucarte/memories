5f6c4c053d428c9cf16c64e3b4342a2fb09c51fb
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-10-15 21:10:57 +0200

    new subdomain

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://api-memories.vercel.app', 'https://mypearlsofgreatprice.vercel.app' ], 
+    'allowed_origins' => ['https://api.mypearlsofgreatprice.de', 'https://mypearlsofgreatprice.de' ], 
 
     // 'allowed_origins' => ['*'],
 

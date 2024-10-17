dde6acf609ab6a936b5a4d87986a03b2809972d2
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-10-17 17:29:41 +0200

    update CORS

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://api.mypearlsofgreatprice.de', 'https://mypearlsofgreatprice.de' ], 
+    'allowed_origins' => ['https://api.mypearlsofgreatprice.de', 'https://mypearlsofgreatprice.de', 'www.mypearlsofgreatprice.de' ], 
 
     // 'allowed_origins' => ['*'],
 

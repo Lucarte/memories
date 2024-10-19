1618f869f3c5ececf6a62217cb5f74db85240a6b
LucArte <96082778+Lucarte@users.noreply.github.com>
2024-10-19 17:43:19 +0200

    Add CategoriesSeeder and migrations

---
+++ database/seeders/CategoriesSeeder.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategoriesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            'Music',
+            'Sports',
+            'Dance',
+            'Viola',
+            'Musical Theater',
+            'Programming',
+            'Art',
+            'Various',
+            'Climbing',
+            'Running',
+            'Swimming',
+            'Harmonica',
+            'IJK',
+            'FJO',
+            'CMS',
+            'Theater',
+            'Horse Riding',
+            'Meditation',
+            'Cold Plunges',
+            'Primary School',
+            'Around-the-World',
+            'Turnen',
+        ];
+
+        foreach ($categories as $category) {
+            DB::table('categories')->insert([
+                'category' => $category,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
+    }
+}
